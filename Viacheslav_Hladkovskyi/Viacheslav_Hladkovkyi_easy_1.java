306121d71930d6c2ffd6bf4fc3b7af96b52ec15a
waklaw <31567809+waklaw@users.noreply.github.com>
2019-10-24 20:18:05 +0300

    Update Viacheslav_Hladkovkyi_easy_1.java

--- Viacheslav_Hladkovskyi/Viacheslav_Hladkovkyi_easy_1.java
+++ Viacheslav_Hladkovskyi/Viacheslav_Hladkovkyi_easy_1.java
@@ -1,1 +1,16 @@
+public class Viacheslav_Hladkovskyi_easy_1 {
+    public static void main( String[] args )
+    {
+        String s = "Today is the happiest day of my life";
+        String[] word = s.split( " " );
+        String rts = " ";
 
+        for ( int i = 0; i < word.length; i++ )
+        {
+            if ( word[i].length() > rts.length() )
+                rts = word[i];
+
+        }
+        System.out.println( rts );
+    }
+}
