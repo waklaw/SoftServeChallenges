c9316e08589e7bfe7e0afdbb6e4843f306b81c0b
waklaw <31567809+waklaw@users.noreply.github.com>
2019-10-24 20:43:20 +0300

    Add files via upload

---
+++ Viacheslav_Hladkovskyi/Viacheslav_Hladkovskyi_easy_3.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Slavik
+ */
+public class Viacheslav_Hladkovskyi_easy_3 {
+   public static void main(String args[]){  
+   int num = 5;
+   int sum = 0;
+   for(int i=0; i<=num; i++){
+       sum = sum+i;
+  }    
+  System.out.println("Result is "+sum);    
+ }
+}
