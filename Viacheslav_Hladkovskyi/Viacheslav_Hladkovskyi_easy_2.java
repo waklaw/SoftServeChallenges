1a02c2f360534e2dd1d6370b77fab9ebfd0ef29b
waklaw <31567809+waklaw@users.noreply.github.com>
2019-10-24 20:28:47 +0300

    Add files via upload

---
+++ Viacheslav_Hladkovskyi/Viacheslav_Hladkovskyi_easy_2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Slavik
+ */
+public class Viacheslav_Hladkovskyi_easy_2 {
+  public static void main(String args[]){  
+  int i,f=1;  
+  int number=4;    
+  for(i=1;i<=number;i++){    
+      f=f*i;    
+  }    
+  System.out.println("Factorial of "+number+" is: "+f);    
+ }
+}
